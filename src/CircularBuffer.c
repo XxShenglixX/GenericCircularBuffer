f5232799a219094d51765c689134431f4fd3dc67
XxShenglixX <c_shengli@hotmail.com>
2014-06-28 19:08:48 +0800

    Added comments

--- src/CircularBuffer.c
+++ src/CircularBuffer.c
@@ -44,6 +44,17 @@ void circularBufferDelete(CircularBuffer *circularBuffer)
 	free(circularBuffer);
 }
 
+
+/* *
+ * Add data to the circular buffer 
+ * Note : Will throw an error if the buffer is full
+ *
+ * Input : 
+ *	cb is the circular buffer
+ *	obj contain the address of the value needed to be added to the circular buffer
+ *  copy contain the address for copy double/integer operation
+ *
+ */
 void circularBufferAdd(
 						CircularBuffer *cb,
 						void *obj,
@@ -63,7 +74,16 @@ void circularBufferAdd(
 		cb->tail = (cb->head - cb->sizeOfType); // tail is 1 step behind of head
 }				
 
-
+/* *
+ * Remove data from the circular buffer
+ * Note : Will throw an error if the buffer is empty
+ *
+ * Input : 
+ *	cb is the circular buffer
+ *	obj contain the address of result which will stored the removed data from the circular buffer
+ *  copy contain the address for copy double/integer operation
+ *
+ */
 void circularBufferRemove(
 						CircularBuffer *cb,
 						void *obj,
@@ -80,7 +100,15 @@ void circularBufferRemove(
 
 }
 		
-
+/* *
+ * Check if the buffer is empty
+ *
+ * Input : 
+ *	circularBuffer is the circular buffer
+ *	
+ * Output :
+ *	1 if buffer is empty, and 0 for not empty buffer
+ */
 int circularBufferIsEmpty(CircularBuffer *circularBuffer)
 {
 	if (circularBuffer->size <=0 )
@@ -89,6 +117,15 @@ int circularBufferIsEmpty(CircularBuffer *circularBuffer)
 		return 0 ;
 }
 
+/* *
+ * Check if the buffer is full
+ *
+ * Input : 
+ *	circularBuffer is the circular buffer
+ *	
+ * Output :
+ *	1 if buffer is full, and 0 for buffer that is not full yet
+ */
 int circularBufferIsFull(CircularBuffer *circularBuffer)
 {
 	if (circularBuffer->size >= circularBuffer->length )
